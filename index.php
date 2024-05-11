8755f1c83c470a0d9d8cc0cd86da8e19d8052845
AvinaashP2010 <avinaashponnada290@gmail.com>
2024-05-11 16:56:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ button:hover {
         <h2>Login</h2>
         <form id="loginForm">
             <div class="form-group">
-                <label for="username">Username</label>
+                <label for="username">Username:</label>
                 <input type="text" id="username" name="username" required>
             </div>
             <div class="form-group">
